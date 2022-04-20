9a452f428c55ae4612ecf3c9e39c9e0ca9bf3912
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-04-20 10:02:55 -0500

    querires r=created

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,11 +7,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello World!'
-//     });
-// });
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your MySql username,
+        user: 'root',
+        //Your MySql password
+        password: 'Veronichka88$$',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
